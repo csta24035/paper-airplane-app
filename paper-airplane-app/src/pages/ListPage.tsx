d28e87a78c148ce94cd30ff29f6b088a8234f55b
csta24035 <csta24035@g.nihon-u.ac.jp>
2026-07-01 18:35:33 +0900

    タイトル変更

--- paper-airplane-app/src/pages/ListPage.tsx
+++ paper-airplane-app/src/pages/ListPage.tsx
@@ -2,12 +2,17 @@ type Props = {
   children: React.ReactNode;
 };
 
-function ListPage({
-  children,
-}: Props) {
+function ListPage({ children }: Props) {
   return (
     <div>
-      <h1>紙飛行機記録アプリ</h1>
+      <h1>
+        GlideLog
+        <br />{/* 1回目の改行 */}
+        <br />{/* 2回目の改行 */}
+        <span style={{ display: 'block', fontSize: '0.6em', fontWeight: 'normal', color: '#666' }}>
+          紙飛行機を一括管理
+        </span>
+      </h1>
 
       {children}
     </div>
